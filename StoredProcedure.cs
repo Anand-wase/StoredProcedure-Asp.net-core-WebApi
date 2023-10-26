0bc5bb3a9abfbe64cf4b13f0d93e8578d19993ea
Anand-wase <anandvardhan.16pav@gmail.com>
2023-10-26 13:41:23 +0530

    Added Stored Procedure Code to write in SSMS

---
+++ StoredProcedure.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using StoredProcedure.Models;
+
+namespace StoredProcedure
+{
+    public class StoredProcedure
+    {
+        //Stored Procedure code to be written in ssms
+
+
+                //Create proc[dbo].[SP_EMPLOYEE]
+                //@Id int = 0,
+                //@Name nvarchar(max)=" ",
+                //@Email nvarchar(max)=" ",
+                //@Designation nvarchar(max)=" ",
+                //@Type nvarchar(max)=NULL
+                //as
+                //begin
+                //if (@Type='GetAll')
+                //begin
+                //select* from Employes;
+                //end
+                //else if (@Type='GetById')
+                //begin
+                //select* from Employes where Id=@Id;
+                //end
+                //else if (@Type='delete')
+                //begin
+                //delete from Employes where Id = @Id;
+                //        end
+                //else if (@Type='Insert')
+                //begin
+                //insert into Employes values(@Name , @Email, @Designation , ' ');
+                //        end
+ 
+                //else if (@Type='Update')
+                //begin
+                //UPDATE Employes
+                //SET Email = @Email, Name = @Name, Designation = @Designation WHERE Id = @Id;
+                //end
+
+
+                //end
+    }
+}
